4f8499051cf2f1b65c15681020b382a7f500e71b
maro <kamanbe0120@gmail.com>
2020-11-13 04:42:18 +0000

    a

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
-import * as React from "react"
 import '../styles/global.css'
 
 export default function App({ Component, pageProps }) {
-  return /*#__PURE__*/React.createElement(Component, pagePros);
+  return <Component {...pageProps} />
 }
